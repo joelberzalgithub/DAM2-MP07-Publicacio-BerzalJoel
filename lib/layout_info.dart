56669e7efd3a80cee64a7d4b22c9e97e17954068
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-05-04 11:18:43 +0200

    Afegides classes principals

---
+++ lib/layout_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LayoutInfo extends StatefulWidget {
+  const LayoutInfo({Key? key}) : super(key: key);
+
+  @override
+  LayoutInfoState createState() => LayoutInfoState();
+}
+
+class LayoutInfoState extends State<LayoutInfo> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/layout_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LayoutMenu extends StatefulWidget {
+  const LayoutMenu({Key? key}) : super(key: key);
+
+  @override
+  LayoutMenuState createState() => LayoutMenuState();
+}
+
+class LayoutMenuState extends State<LayoutMenu> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
